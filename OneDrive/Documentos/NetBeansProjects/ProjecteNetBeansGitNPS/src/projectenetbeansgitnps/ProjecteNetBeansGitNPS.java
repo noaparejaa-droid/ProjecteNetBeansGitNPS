05c403e4cab3610ac388f7e1b51ab6eac845947b
noa <noaparejaa@gmail.com>
2025-12-03 18:46:41 +0100

    Segon commit: modificació al Main

--- OneDrive/Documentos/NetBeansProjects/ProjecteNetBeansGitNPS/src/projectenetbeansgitnps/ProjecteNetBeansGitNPS.java
+++ OneDrive/Documentos/NetBeansProjects/ProjecteNetBeansGitNPS/src/projectenetbeansgitnps/ProjecteNetBeansGitNPS.java
@@ -14,7 +14,8 @@ public class ProjecteNetBeansGitNPS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("Modificació 1");
+ // TODO code application logic here
     }
     
 }
